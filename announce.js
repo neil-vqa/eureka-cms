43bbaa9057916f016d72b39151e12f44f2e7cfbe
Neil Alino <nvq.alino@gmail.com>
2020-11-09 15:36:30 +0800

    revert to processmd

--- announce.js
+++
@@ -1,9 +0,0 @@
-var jsonConcat = require("json-concat");
-
-jsonConcat({
-    src: './content/announcement',
-    dest: './content/announcement/index.json'
-}, function (json, err) {
-    console.log(json);
-     console.log(err);
-});
